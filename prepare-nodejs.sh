867fb97fc52e0ea077e44186c6bc02ebcc1623d2
muhaylosemenyuk <79005788+muhaylosemenyuk@users.noreply.github.com>
2022-12-30 23:50:26 +0200

    Create prepare-nodejs.sh

---
+++ prepare-nodejs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update
+if [ ! -f "/root/.nvm/versions/node/v16.18.0/bin/node" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+  touch $HOME/.bash_profile
+  echo 'export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.bash_profile
+  . $HOME/.bash_profile
+  nvm install 16.18.0 && node -v
+fi
